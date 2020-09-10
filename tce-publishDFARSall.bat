8da3c479f5f3c7d17699060135d07ff18755aa9d
ThomasMAldous <Tom.aldous@thecontentera.com>
2020-09-10 12:36:14 -0400

    Removed some rev attributes notes I created while troubleshooting and added publish bat

---
+++ tce-publishDFARSall.bat
@@ -0,0 +1,3 @@
+
+C:\GSA-Acquisition-Tools\TOOLS\DITA-OT\dita-ot-2.5.4\bin\dita  -i "C:\GSA_Regs\GSA-Acquisition-DFARS\dita\DfarsAppendixPGICombined.ditamap" -f gsa-dfars.pdf -o "C:\GSA_Regs\GSA-Acquisition-DFARS\pdf" > "C:\GSA_Regs\GSA-Acquisition-DFARS\dita\PGI-publish-log.txt" 2>&1
+
