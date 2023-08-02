57f2aeeb85fa5d382feceb23f2a49be7f7e082de
GregPangborn <gregory.pangborn@gsa.gov>
2023-08-02 14:19:51 -0400

    Changes per 7/20 publication and repo clean up

--- tce-publishDFARSall.bat
+++
@@ -1,3 +0,0 @@
-
-C:\GSA-Acquisition-Tools\TOOLS\DITA-OT\dita-ot-2.5.4\bin\dita  -i "C:\GSA_Regs\GSA-Acquisition-DFARS\dita\DfarsAppendixPGICombined.ditamap" -f gsa-dfars.pdf -o "C:\GSA_Regs\GSA-Acquisition-DFARS\pdf" > "C:\GSA_Regs\GSA-Acquisition-DFARS\dita\DfarsAppendixPGICombined-publish-log.txt" 2>&1
-
--- tce-publishPGI.bat
+++
@@ -1,3 +0,0 @@
-
-C:\GSA-Acquisition-Tools\TOOLS\DITA-OT\dita-ot-2.5.4\bin\dita  -i "C:\GSA_Regs\GSA-Acquisition-DFARS\dita\PGI.ditamap" -f gsa-dfars.pdf -o "C:\GSA_Regs\GSA-Acquisition-DFARS\pdf" > "C:\GSA_Regs\GSA-Acquisition-DFARS\dita\PGI-publish-log.txt" 2>&1
-
